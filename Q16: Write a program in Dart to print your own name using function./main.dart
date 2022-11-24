fe4f535346a66eed73b24ba55c78b03c96c7243d
Muneeb Shahid <115135886+muneeb-shahid@users.noreply.github.com>
2022-11-24 09:06:36 -0800

    Create main.dart

---
+++ Q16: Write a program in Dart to print your own name using function./main.dart
@@ -0,0 +1,9 @@
+// Q: Write a program in Dart to print your own name using function.
+
+void main(List<String> args) {
+  print("My name is: ${name("Muneeb Shahid")}");
+}
+
+String name(String name) {
+  return name;
+}
