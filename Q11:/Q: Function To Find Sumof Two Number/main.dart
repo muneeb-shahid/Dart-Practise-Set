e66d67aaed0f26f054cc9626b706103b7231f40c
Muneeb Shahid <115135886+muneeb-shahid@users.noreply.github.com>
2022-11-24 09:02:35 -0800

    Create main.dart

---
+++ Q11:/Q: Function To Find Sumof Two Number/main.dart
@@ -0,0 +1,9 @@
+// Q: Function To Find Sum of Two Numbers
+void main(List<String> args) {
+  print("The addition of two numbers is: ${addition(2.231, 3.435)}");
+}
+
+num addition(num1, num2) {
+  num sum = num1 + num2;
+  return sum;
+}
