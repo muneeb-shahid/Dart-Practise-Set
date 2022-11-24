a458cdb435f0b1a03fc957cd5b624148744c2672
Muneeb Shahid <115135886+muneeb-shahid@users.noreply.github.com>
2022-11-24 09:04:14 -0800

    Delete Q11:/Q: Function To Find Sumof Two Number directory

--- Q11:/Q: Function To Find Sumof Two Number/main.dart
+++
@@ -1,9 +0,0 @@
-// Q: Function To Find Sum of Two Numbers
-void main(List<String> args) {
-  print("The addition of two numbers is: ${addition(2.231, 3.435)}");
-}
-
-num addition(num1, num2) {
-  num sum = num1 + num2;
-  return sum;
-}
